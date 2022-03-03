55e1ef37b8507f70215aa92bfe5b6eb2ab5e1b58
王宇航-hz19114716 <hz19114716@huize.com>
2022-03-03 15:29:01 +0800

    fix:delete dva test code

--- src/models/todoList.ts
+++ src/models/todoList.ts
@@ -1,5 +1,3 @@
-import type { AnyAction } from 'redux';
-
 export interface TodoListProps {
   todoList: string[];
 }
@@ -13,27 +11,7 @@ const TodoList = {
     // 此处为saga异步操作的编写方式，用来声明异步操作的方法
   },
   reducers: {
-    setTodoList(state: TodoListProps, action: AnyAction) {
-      const _state = Object.assign({}, state);
-      const { type, value } = action.payload;
-      const todoList = [..._state.todoList];
-      const flag = todoList.indexOf(value);
-
-      if (type === 'add') {
-        // 新增
-        if (flag > -1) {
-          todoList.splice(flag, 1);
-        }
-        todoList.push(value);
-      } else {
-        todoList.splice(flag, 1);
-      }
-
-      return {
-        ..._state,
-        todoList,
-      };
-    },
+    // reduders，修改state
   },
   // subscriptions:{
   //   setup({ dispatch, history }) {
