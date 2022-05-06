52393f486dc13d1180ccb82b7c0a887f0a3ae6c4
王宇航-hz19114716 <hz19114716@huize.com>
2022-05-06 10:07:35 +0800

    feat(新增代码提交规范): 新增代码提交规范

---
+++ .commitlintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // parserPreset: './.parser-preset.js',
+  extends:['@commitlint/config-conventional'],
+  rules: {
+    // 'type-empty': [2, 'never'],
+    // 'type-case': [2, 'always', 'lower-case'],
+    // 'subject-empty': [2, 'never'],
+    // 'type-enum': [
+    //   2,
+    //   'always',
+    //   [
+    //     'feat',
+    //     'fix',
+    //     'docs',
+    //     'style',
+    //     'refactor',
+    //     'test',
+    //     'revert',
+    //     'build',
+    //     'ci',
+    //     'chore',
+    //     'perf',
+    //   ],
+    // ],
+  },
+};
